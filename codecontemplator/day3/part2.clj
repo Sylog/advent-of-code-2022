beaf151b341b1e974124115326107d34d477271a
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2022-12-03 14:59:40 +0100

    cc d3 p2

---
+++ codecontemplator/day3/part2.clj
@@ -0,0 +1,31 @@
+(require ['clojure.string :as 'str])
+(require ['clojure.set :as 'nsset])
+
+
+(defn readFile [fn]
+  (def text (slurp fn))
+  (partition 3 (str/split-lines text)))
+
+(defn splitInHalf [s]
+    (def half (/ (count s) 2))
+    [(subs s 0 half) (subs s half)])
+
+(defn priority [ch]
+  (if 
+    (Character/isUpperCase ch)    
+    (+ (- (int ch) (int \A)) 27)
+    (+ (- (int ch) (int \a)) 1)))
+
+(defn priorityGroup [[str1 str2 str3]]
+  (let [overlap (nsset/intersection (set str1) (set str2) (set str3))
+        prios (map priority overlap)
+        ]
+  (reduce + prios)))
+
+(defn solve [fn]
+  (let [groups (readFile fn)]
+    (reduce +
+        (map priorityGroup groups))))
+
+(println (solve "sample.txt"))
+(println (solve "input.txt"))
