39851fa8937134552c738a41889857811d44264e
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2022-12-14 20:14:21 +0100

    cc d13 cleaup

--- codecontemplator/day 13/Main.hs
+++ codecontemplator/day 13/Main.hs
@@ -1,12 +1,9 @@
 module Main where
 
 import Text.Parsec
-import Debug.Trace
 import Data.List.Split (splitOn)
 
 data NestedInts = I Int | L [NestedInts] deriving Show
-
-example = "[1,[2,[3,[4,[5,6,7]]]],8,19]"
 
 pint:: Parsec String st Int
 pint = read <$> many1 digit
@@ -18,8 +15,7 @@ plist =
     (L <$> between (char '[') (char ']') (sepBy plist (char ',')))
     
 compareL :: NestedInts -> NestedInts -> Ordering 
-compareL (I i1) (I i2) = -- trace (show i1 ++ " ~ " ++ show i2) $ 
-    compare i1 i2
+compareL (I i1) (I i2) = compare i1 i2
 compareL (I i) l = compareL (L [I i]) l
 compareL l (I i) = compareL l (L [I i])
 compareL (L []) (L []) = EQ
